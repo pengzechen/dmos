705042ab7034d4de71bcf5c18829c0fa68509eea
pzc <927068267@qq.com>
2023-09-19 22:55:53 +0800

    将libapp做成库文件

--- src/applib/lib_syscall.h
+++ src/applib/lib_syscall.h
@@ -12,78 +12,18 @@ typedef struct _syscall_arg_t {
 } syscall_arg_t ;
 
 
+void  msleep(int ms);
 
-static inline int 
-sys_call (syscall_arg_t * args) {
-    int ret;
-	volatile uint32_t addr[] = {0,  SELECTOR_SYSCALL | 0};
-	__asm__ __volatile__(
-        "push %[arg3]\n\t"
-        "push %[arg2]\n\t"
-        "push %[arg1]\n\t"
-        "push %[arg0]\n\t"
-        "push %[id]\n\t"
-        "lcalll *(%[a])"       
-        :"=a"(ret)              //  output
-        :[arg3]"r"(args->arg3), //  input
-        [arg2]"r"(args->arg2), 
-        [arg1]"r"(args->arg1), 
-        [arg0]"r"(args->arg0), 
-        [id]"r"(args->id), 
-        [a]"r"(addr)
-    );
+int getpid();
 
-    return ret;
-}
+void print_msg (const char*fmt, int arg);
 
+int fork();
 
-static inline void 
-msleep(int ms) {
-    if(ms <= 0) { return; }
-    syscall_arg_t args;
-    args.id = SYS_sleep;
-    args.arg0 = ms;
-    sys_call(&args);
-}
+int execve(const char* name, 
+    char* const* argv, char* const * env );
 
-static inline int
-getpid() {
-    syscall_arg_t args;
-    args.id = SYS_getpid;
-    return sys_call(&args);
-}
+int yield();
 
-static inline void 
-print_msg (const char*fmt, int arg) {
-    syscall_arg_t args;
-    args.id = SYS_print_msg;
-    args.arg0 = (int)fmt;
-    args.arg1 = arg;
-    sys_call(&args);
-}
-
-static inline int
-fork() {
-    syscall_arg_t args;
-    args.id = SYS_fork;
-    return sys_call(&args);
-}
-
-static inline int
-execve(const char* name, char* const* argv, char* const * env ) {
-    syscall_arg_t args;
-    args.id = SYS_execve;
-    args.arg0 = (int)name;
-    args.arg1 = (int)argv;
-    args.arg2 = (int)env;
-    return sys_call(&args);
-}
-
-static inline int
-yield() {
-    syscall_arg_t args;
-    args.id = SYS_yield;
-    return sys_call(&args);
-}
 
 #endif  // LIB_SYSCALL_H
